2b1e5e6c4b5891e4c8a76dd70372fa034f927549
Alex <aleks.bochar@gmail.com>
2019-10-19 01:41:26 +0300

    DOM elements are created, labels on the map are not reflected

--- js/main.js
+++ js/main.js
@@ -1,28 +1,17 @@
 var MapSite = document.querySelector('.map');
 MapSite.classList.remove('map--faded');
 
-var mapElement = document.querySelector('.map');
-var cardTemplate = document.querySelector('#card')
-.content
-.querySelector('article');
-
-for (var i = 0; i < adsDescription.length; i++) {
-  var cardElement = cardTemplate.cloneNode(true);
-
-  mapElement.appendChild(cardElement);
-}
-
-var ads = [1, 2, 3, 4, 5, 6, 7, 8];
 var adsDescription = [];
 
-for(var i = 0; i < 8; i++) {
-  var House = ['palace', 'flat', 'house', 'bungalo'];
-  var TypeHouse = House[Math.floor(Math.random() * House.length)];
-  var Time = ['12.00', '13.00', '14.00'];
-  var TimeCheck = Time[Math.floor(Math.random() * Time.length)];
-  var Feature = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-  var Photo = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+var House = ['palace', 'flat', 'house', 'bungalo'];
+var TypeHouse = House[Math.floor(Math.random() * House.length)];
+var Time = ['12.00', '13.00', '14.00'];
+var TimeCheck = Time[Math.floor(Math.random() * Time.length)];
+var Feature = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+var Photo = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
   'http://o0.github.io/assets/images/tokyo/hotel3.jpg']
+
+for (var i = 0; i < 8; i++) {
 
   adsDescription.push({
     author: {
@@ -42,10 +31,38 @@ for(var i = 0; i < 8; i++) {
       photos: Photo,
     },
     location: {
-      X: Math.floor(Math.random() * (1275 - 75) ) + 75,
-      Y: Math.floor(Math.random() * (630 - 130) + 130,
-    },
+      x: Math.floor(Math.random() * (1275 - 75) ) + 75,
+      y: Math.floor(Math.random() * (630 - 130) + 130,
+    }
   })
 }
 
+var mapElement = document.querySelector('.map__pin');
+var cardTemplate = document.querySelector('#card')
+.content
+.querySelector('article');
+var fragment = document.createDocumentFragment();
 
+for (var i = 0; i < adsDescription.length; i++) {
+  var cardElement = cardTemplate.cloneNode(true);
+  var styleString = 'left: ' + advCard.location.x + ';' + 'top: ' + advCard.location.y;
+  var advCard = adsDescription[i];
+
+  cardElement.querySelector('.popup__avatar').src = advCard.author.avatar;
+  cardElement.querySelector('.popup__avatar').style = styleString;
+  cardElement.querySelector('.popup__title').src = advCard.offer.title;
+  cardElement.querySelector('.popup__text--address').src = advCard.offer.address;
+  cardElement.querySelector('.popup__text--price').src = advCard.offer.price;
+  cardElement.querySelector('.popup__type').src = advCard.offer.type;
+  cardElement.querySelector('.popup__text--capacity').src = advCard.offer.rooms;
+  cardElement.querySelector('.popup__text--capacity').src = advCard.offer.guests;
+  cardElement.querySelector('.popup__text--time').src = advCard.offer.checkin;
+  cardElement.querySelector('.popup__text--time').src = advCard.offer.checkout;
+  cardElement.querySelector('.popup__features').src = advCard.offer.features;
+  cardElement.querySelector('.popup__description').src = advCard.offer.description;
+  cardElement.querySelector('.popup__photos').src = advCard.offer.photos;
+
+  fragment.appendChild(cardElement);
+}
+
+mapElement.appendChild(fragment);
